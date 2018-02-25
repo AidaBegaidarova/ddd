1943c7a159fbd3a334e65bdf4512f1096ce40fed
erv <indorm1992@mail.ru>
2018-02-26 00:36:30 +0300

    change some files to pass task

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def wsgi_app(environ, start_response):
     status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
     resp = environ['QUERY_STRING'].split('&') 
-    resp = [item+'\n\r' for item in resp]
+    resp = [item.encode('utf-8')+b'\r\n' for item in resp]
+    headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
     return resp
--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo rm /etc/nginx/sites-enabled/default
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
+sudo killall gunicorn
 sudo gunicorn -c etc/gunicorn.conf.py hello:wsgi_app -D
 #sudo /etc/init.d/gunicorn restart
